54c3a9a9b702811832db43af13c1a4ffedbfe7b3
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-06-28 14:31:15 +0200

    Improved fading  performance

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -49,9 +49,53 @@ $(function() {
     fadeElem.each(function() {
       var $this = $(this),
       elemOffsetTop = $this.offset().top;
-      if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 -(currScrollPos-elemOffsetTop)/1000);
+      if (currScrollPos >= elemOffsetTop) $this.css('opacity', 1 -(currScrollPos-elemOffsetTop)/600);
     });
   });
+});
+
+(function () {
+
+    var $select = $('#header');
+    var $window = $(window);
+    var isFixed = false;
+    var init = $select.length ? $select.offset().top : 0;
+
+    var $jumbo = $('.jumbo');
+    var jumboHeight = $jumbo.height();
+
+    $window.scroll(function () {
+        var currentScrollTop = $window.scrollTop();
+
+        var currentJumboAlpha = Math.max(1 - ((currentScrollTop * 100 / jumboHeight) / 100), 0);
+
+        $jumbo.css('opacity', currentJumboAlpha);
+
+        if (currentScrollTop > init && isFixed === false) {
+            isFixed = true;
+            $select.css({
+                top: 0,
+                position: 'fixed'
+            });
+            $('body').css('padding-top', $select.height());
+        } else if (currentScrollTop <= init && isFixed === true) {
+            isFixed = false;
+            $select.css('position', 'relative');
+
+            $('body').css('padding-top', 0);
+        }
+    });
+
+    $(".nav").click(function (e) {
+        e.preventDefault();
+        var divId = $(this).attr('href');
+        $('body').animate({
+            scrollTop: $(divId).offset().top - $select.height()
+        }, 500);
+    });
+
+
+
 });
 
 $(function() {
@@ -64,7 +108,7 @@ $(function() {
     footerElem.each(function() {
       var $this = $(this),
       footerOffsetTop = $this.offset().top;
-      if (footerScrollPos < footerOffsetTop) $this.css('opacity', 1.38 +(footerScrollPos-footerOffsetTop)/1000);
+      if (footerScrollPos < footerOffsetTop) $this.css('opacity', 1.38 +(footerScrollPos-footerOffsetTop)/900);
     });
   });
 });
