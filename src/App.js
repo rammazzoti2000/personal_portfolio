3faa3ef95b64bd26ca53b253ed0e7923b99b0a86
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-10-08 21:21:22 +0200

    Small fixes in contact component

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
         this.setState({resumeData: data});
         setTimeout(()=>{
           load.outerHTML='';
-        },2000)
+        },1250)
       }.bind(this),
       error: function(xhr, status, err){
         console.log(err);
