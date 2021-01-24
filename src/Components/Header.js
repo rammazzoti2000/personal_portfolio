d7f2b0062964e6c0e15dba4bb2b654056b121c34
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-01-24 03:43:54 +0100

    Fixing https://github.com/rammazzoti2000/personal_portfolio/issues/35

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -19,7 +19,7 @@ class Header extends Component {
     }
 
     return (
-      <header id="home">
+      <header>
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
             Show navigation
