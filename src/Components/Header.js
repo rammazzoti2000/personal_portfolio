fa9178074a2a5f0aff6db3619dc0a84866b390f5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-01-24 21:13:23 +0100

    Revert Header.js to how it was before

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,24 +13,12 @@ class Header extends Component {
    }
 
     return (
-     <header>
+     <header id="home">
         <nav id="nav-wrap">
            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
            <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
            <ul id="nav" className="nav">
-           <li className="current">
-              <a            
-                className="smoothscroll"
-                onClick={() => {
-                  window.scrollTo({
-                    top: 0,
-                    behavior: 'smooth',
-                  });
-                }}
-              >
-                Home
-              </a>
-            </li>
+              <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
               <li><a className="smoothscroll" href="#about">About</a></li>
               <li><a className="smoothscroll" href="#resume">Resume</a></li>
               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
