418f138cf2af16ebd838855eabdf4ad62d58242a
Alex <bangau.alexandru@gmail.com>
2021-03-04 01:29:16 +0100

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,8 +17,11 @@ class Footer extends Component {
               {networks}
             </ul>
             <ul className="copyright">
-              <li>&copy; Copyright 2020 Alex Bangau</li>
-              <li>Design by <a href="https://github.com/rammazzoti2000">ale[x360]</a></li>
+              <li>&copy; Copyright {new Date().getFullYear()} Alex Bangau</li>
+              <li>
+                Design by{" "}
+                <a href="https://github.com/rammazzoti2000">ale[x360]</a>
+              </li>
             </ul>
             </div>
           <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
